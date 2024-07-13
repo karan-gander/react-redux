6ce9e599d66134f4bfe5af31cffa4e24f6d11fb1
karan-gander <meghawalkaran@gmail.com>
2024-07-13 08:58:06 +0530

    react-redux

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/Addtodo'
+import Todos from './components/Todo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <div>
+    <h1>Learn about redux toolkit</h1>
+    <AddTodo/>
+    <Todos/>
+  </div>
+    </>
+  )
+}
+
+export default App
